4741ccc5fdf393c5036ce0f9c416aea2ef854d20
Tom Solacroup <tom.solacroup@gmail.com>
2016-11-12 01:55:39 +0100

    Added GamepadInput and PlayerCharacter classes

---
+++ Assets/Scripts/GamepadInput.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GamepadInput : MonoBehaviour {
+
+    [SerializeField]
+    PlayerCharacter playerCharacter;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        /////////////////////// AXES ////////////////////////////////
+	    if(Input.GetAxis("P" + playerCharacter.playerIndex + "LeftHorizontal") > 0)
+        {
+            //TODO
+        }
+        else if (Input.GetAxis("P" + playerCharacter.playerIndex + "LeftHorizontal") < 0)
+        {
+            //TODO
+        }
+
+        if (Input.GetAxis("P" + playerCharacter.playerIndex + "LeftVertical") > 0)
+        {
+            //TODO
+        }
+        else if (Input.GetAxis("P" + playerCharacter.playerIndex + "LeftVertical") < 0)
+        {
+            //TODO
+        }
+
+        if (Input.GetAxis("P" + playerCharacter.playerIndex + "RightHorizontal") > 0)
+        {
+            //TODO
+        }
+        else if (Input.GetAxis("P" + playerCharacter.playerIndex + "RightHorizontal") < 0)
+        {
+            //TODO
+        }
+
+        if (Input.GetAxis("P" + playerCharacter.playerIndex + "RightVertical") > 0)
+        {
+            //TODO
+        }
+        else if (Input.GetAxis("P" + playerCharacter.playerIndex + "RightVertical") < 0)
+        {
+            //TODO
+        }
+
+        //////////////////////// BUTTONS ///////////////////////////////
+
+        if (Input.GetButtonDown("P" + playerCharacter.playerIndex + "ButtonA"))
+        {
+            //TODO
+        }
+        if (Input.GetButtonDown("P" + playerCharacter.playerIndex + "ButtonB"))
+        {
+            //TODO
+        }
+        if (Input.GetButtonDown("P" + playerCharacter.playerIndex + "ButtonX"))
+        {
+            //TODO
+        }
+        if (Input.GetButtonDown("P" + playerCharacter.playerIndex + "ButtonY"))
+        {
+            //TODO
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    public int playerIndex;
+
+    public PlayerCharacter(int p)
+    {
+        this.playerIndex = p;
+    }
+
+
+}
