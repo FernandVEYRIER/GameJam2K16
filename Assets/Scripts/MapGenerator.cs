691346284784f6d2a0368e9b63f28797b8460dbe
Fernand <fernand.veyrier@epitech.eu>
2016-11-12 20:03:48 +0100

    added terrain manager

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Obstacle {
+	public GameObject obstaclePrefab = null;
+	public float objDistance = 0;
+
+	public Obstacle(GameObject prefab = null, float dist = 0)
+	{
+		obstaclePrefab = prefab;
+		objDistance = dist;
+	}
+}
+
+public class MapGenerator : MonoBehaviour {
+	
+	private float distance = 0;
+	private List<Obstacle> terrainList = new List<Obstacle> ();
+	private float currVelocity = 1;
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (GameManager.GM.State == Assets.Scripts.GameState.PLAY)
+		{
+			if (terrainList.Count > 0 && terrainList[0].objDistance <= distance)
+			{
+				if (terrainList[0].obstaclePrefab != null)
+				{
+					Instantiate (terrainList [0].obstaclePrefab, transform.position, transform.rotation);
+				}
+				terrainList.RemoveAt (0);
+			}
+			distance += currVelocity;
+		}
+	
+	}
+
+	public void PushTerrain(Obstacle obs)
+	{
+		terrainList.Add (obs);
+	}
+}
--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -1,17 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Assets.Scripts;
 
 public class MapManager : MonoBehaviour {
 
 	[SerializeField] private GameObject[] sides;
+	[SerializeField] private MapGenerator[] spawners;
 	[SerializeField] private float startDist;
 	[SerializeField] private float velocity;
 
-	Vector3[] tr;
+	[SerializeField] private float minSpawnDelay = 2f;
+	[SerializeField] private GameObject obstaclePrefab;
+
+	private Vector3[] tr;
 	private Vector3[] vel;
+	private float currDelay;
+	private float distance = 0;
 
 	// Use this for initialization
 	void Start () {
+		currDelay = minSpawnDelay;
 		if (sides.Length > 0)
 		{
 			tr = new Vector3[sides.Length];
@@ -29,6 +37,28 @@ public class MapManager : MonoBehaviour {
 		for (int i = 0; i <sides.Length; ++i)
 		{
 			sides [i].transform.position = Vector3.SmoothDamp (sides [i].transform.position, tr [i], ref vel[i], velocity);
+		}
+
+		if (GameManager.GM.State == GameState.PLAY)
+		{
+			if (currDelay <= 0)
+			{
+				GenerateMap ();
+				currDelay = minSpawnDelay;
+			}
+			else
+				currDelay -= Time.deltaTime;
+			++distance;
+		}
+	}
+
+	void GenerateMap()
+	{
+		bool isWall = Random.Range (0, 2) == 0;
+
+		foreach (MapGenerator m in spawners)
+		{
+			m.PushTerrain (new Obstacle((isWall) ? obstaclePrefab : null, distance));
 		}
 	}
 }
