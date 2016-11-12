09bd50ac61ab193ee8b0e264e8de91089ccef437
Fernand <fernand.veyrier@epitech.eu>
2016-11-12 01:58:50 +0100

    Aded powerup

---
+++ Assets/Scripts/APowerUp.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class APowerUp : MonoBehaviour, IPowerUp {
+
+	[SerializeField] private int _remainingUsages;
+
+	public bool Use ()
+	{
+		if (_remainingUsages <= 0)
+			return false;
+		--_remainingUsages;
+		return true;
+	}
+
+	public int RemainingUsages {
+		get {
+			return _remainingUsages;
+		}
+	}
+
+	public void Add()
+	{
+		++_remainingUsages;
+	}
+}
---
+++ Assets/Scripts/IPowerUp.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IPowerUp {
+
+	int RemainingUsages {
+		get;
+	}
+
+	bool Use ();
+	void Add ();
+}
