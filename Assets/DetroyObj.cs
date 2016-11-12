7645f88b510b321e434f0d30a6fefc3dbb8ed7ef
Nicolas-Constanty <nicolas.constanty@epitech.eu>
2016-11-12 14:16:52 +0100

    Fix collider2D

--- Assets/DetroyObj.cs
+++ Assets/DetroyObj.cs
@@ -3,13 +3,11 @@ using System.Collections;
 
 public class DetroyObj : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Obstacle"))
+        {
+            Destroy(col.gameObject);
+        }
+    }
 }
