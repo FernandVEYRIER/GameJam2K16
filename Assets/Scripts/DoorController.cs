0ebf6ca6a0973ae18ff2528d79c822aced354013
Nicolas Constanty <nicolas.constanty@epitech.eu>
2016-11-20 17:33:43 +0100

    Add door

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorController : MonoBehaviour {
+
+    private enum State { none, open, close };
+    private State st;
+    private Quaternion targetRotation;
+    public float speed = 1;
+
+    void Start()
+    {
+        Invoke("Open", 2.5f);
+        targetRotation = transform.localRotation;
+    }
+
+    public void Close()
+    {
+       st = State.close;
+    }
+
+    public void Open()
+    {
+        st = State.open;
+    }
+
+    void Update()
+    {
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, Time.deltaTime * speed);
+        if (transform.localRotation == targetRotation)
+        {
+            switch (st)
+            {
+                case State.open:
+                    targetRotation = Quaternion.Euler(0, 0, -120);
+                    st = State.none;
+                    break;
+                case State.close:
+                    targetRotation = Quaternion.Euler(0, 0, 0);
+                    Invoke("Open", 1f);
+                    st = State.none;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PowerUpDoor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts;
+
+public class PowerUpDoor : APowerUp {
+
+    override public bool Use(Direction dir = Direction.RIGHT, int sender = -1, int target = -1)
+    {
+        if (base.Use(dir, sender, target))
+        {
+            ((GameManager)GameManager.GM).CloseDoor(sender);
+            return true;
+        }
+        return false;
+    }
+}
